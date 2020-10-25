1e5f300846faca6a479814af1ff4e4bc10b4e0d1
Martin <martinyanez420@gmail.com>
2020-10-25 09:51:36 -0300

    fixed add buttons

--- js/script.js
+++ js/script.js
@@ -12,8 +12,38 @@ document.querySelector("#addIceBox").addEventListener("click", function(e) {
 */
 
 $(document).ready(function() {
-    $("button.btn").on("click", function() {
+    $("button#addIceBox").on("click", function() {
         $("div.cardContainer1").after(`
+        <div class="newCard">
+        <input>
+        </div>
+        `);
+    });
+});
+
+$(document).ready(function() {
+    $("button#addToDo").on("click", function() {
+        $("div.cardContainer2").after(`
+        <div class="newCard">
+        <input>
+        </div>
+        `);
+    });
+});
+
+$(document).ready(function() {
+    $("button#addDoing").on("click", function() {
+        $("div.cardContainer3").after(`
+        <div class="newCard">
+        <input>
+        </div>
+        `);
+    });
+});
+
+$(document).ready(function() {
+    $("button#addDone").on("click", function() {
+        $("div.cardContainer4").after(`
         <div class="newCard">
         <input>
         </div>
